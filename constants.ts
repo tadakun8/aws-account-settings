71ac3ed4d2bb5d7edc0b67c3efb33cb9c424d908
tadakun8 <tadakun8.lab@gamil.com>
2021-05-01 17:04:18 +0900

    refactor: create a constants file

---
+++ constants.ts
@@ -0,0 +1,16 @@
+/**
+ * The constants for this project are defined here
+ */
+
+export const PREFIX = {
+  PROJECT: "my",
+  PORPUSE: "settings",
+};
+
+export const RESOURCE_PREFIX = `${PREFIX.PROJECT}-${PREFIX.PORPUSE}`;
+
+export const RESOURCE_NAME = {
+  STACK: "stack",
+  BUCKET: "bucket",
+  KEY: "key",
+};
