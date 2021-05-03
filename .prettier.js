b8a3cd6f2e339547fed64c51484a5af372164eb9
tadakun8 <tadakun8.lab@gamil.com>
2021-05-03 22:16:07 +0900

    chore: change printWidth of prettier

--- .prettier.js
+++ .prettier.js
@@ -1,7 +1,8 @@
 // The following are the default settings of prettier.
 // comments out only the parts you want to change and modify them.
 module.exports = {
-    printWidth: 80,
+    // printWidth: 80,
+    printWidth: 120,
     tabWidth: 2,
     useTabs: false,
     semi: true,
