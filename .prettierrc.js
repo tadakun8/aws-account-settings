05b884ee5087a93cef104d308614d1560bffc769
tadakun8 <tadakun8.lab@gamil.com>
2021-05-06 19:42:46 +0900

    fix: modify config file of prettier

---
+++ .prettierrc.js
@@ -0,0 +1,27 @@
+// The following are the default settings of prettier.
+// comments out only the parts you want to change and modify them.
+module.exports = {
+  // printWidth: 80,
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "none",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  parser: "typescript",
+  filepath: "none",
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  vueIndentScriptAndStyle: false,
+  endOfLine: "auto",
+  embeddedLanguageFormatting: "auto"
+};
