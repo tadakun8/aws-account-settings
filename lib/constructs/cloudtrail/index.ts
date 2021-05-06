f0b38cb8b9dd8775299e0160c4a1738116ceb1f4
tadakun8 <tadakun8.lab@gamil.com>
2021-05-06 14:38:11 +0900

    feat: encryption for cloudtrail

--- lib/constructs/cloudtrail/index.ts
+++ lib/constructs/cloudtrail/index.ts
@@ -1,10 +1,12 @@
 import * as cdk from "@aws-cdk/core";
 import * as cloudtrail from "@aws-cdk/aws-cloudtrail";
 import * as s3 from "@aws-cdk/aws-s3";
+import * as kms from "@aws-cdk/aws-kms";
 
 interface CloudTrailConstructProps {
   trailName: string;
   bucket: s3.IBucket;
+  encryptionKey: kms.Key;
 }
 
 export class CloudTrailConstruct extends cdk.Construct {
@@ -18,6 +20,7 @@ export class CloudTrailConstruct extends cdk.Construct {
     new cloudtrail.Trail(this, "CloudTrail", {
       trailName: props.trailName,
       bucket: props.bucket,
+      encryptionKey: props.encryptionKey,
     });
   }
 }
--- lib/stacks/cloudtrail-stack.ts
+++ lib/stacks/cloudtrail-stack.ts
@@ -4,6 +4,7 @@ import { createResourceName } from "../util/createResourceName";
 import * as s3 from "@aws-cdk/aws-s3";
 import { BucketConstruct } from "../constructs/s3/bucket";
 import { CloudTrailConstruct } from "../constructs/cloudtrail";
+import * as kms from "@aws-cdk/aws-kms";
 
 interface CloudTrailStackProps extends cdk.StackProps {
   serverAccessLogBucket: s3.IBucket;
@@ -22,26 +23,14 @@ export class CloudTrailStack extends cdk.Stack {
       serverAccessLogsPrefix: bucketName,
     });
 
-    // bucketConstruct.bucket.addToResourcePolicy(
-    //   new iam.PolicyStatement({
-    //     principals: [new ServicePrincipal("cloudtrail.amazonaws.com")],
-    //     actions: ["s3:PutObject"],
-    //     resources: [`${bucketConstruct.bucket.bucketArn}/*`],
-    //   })
-    // );
-
-    // bucketConstruct.bucket.addToResourcePolicy(
-    //   new iam.PolicyStatement({
-    //     sid: "fa",
-    //     principals: [new ServicePrincipal("cloudtrail.amazonaws.com")],
-    //     actions: ["s3:GetBucketAcl"],
-    //     resources: [`${bucketConstruct.bucket.bucketArn}`],
-    //   })
-    // );
+    const key = new kms.Key(this, "KmsConstruct", {
+      alias: "alias/deleted",
+    });
 
     new CloudTrailConstruct(this, "CloudTrailConstruct", {
       trailName: trailName,
       bucket: bucketConstruct.bucket,
+      encryptionKey: key,
     });
   }
 }
