52d3bf324cc68c3ce3d21369828b4c9c3755eed7
tadakun8 <tadakun8.lab@gamil.com>
2021-05-11 18:13:36 +0900

    refactor: deleted guard duty construct

--- lib/stacks/guardduty-stack.ts
+++ lib/stacks/guardduty-stack.ts
@@ -3,35 +3,33 @@ import * as events from "@aws-cdk/aws-events";
 import * as events_targets from "@aws-cdk/aws-events-targets";
 import { RESOURCE_NAME } from "../../constants";
 import { EventRuleConstruct } from "../constructs/eventbridge/event-rule";
-import { GuardDutyConstruct } from "../constructs/guard-duty";
 import { TopicConstruct } from "../constructs/sns/topic";
 import { createResourceName } from "../util/createResourceName";
+import * as guardduty from "@aws-cdk/aws-guardduty";
 
 export class GuardDutyStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new GuardDutyConstruct(this, "GuardDutyConstruct");
+    new guardduty.CfnDetector(this, "CfnDetector", {
+      enable: true
+    });
 
     const topicName = createResourceName("guardduty", RESOURCE_NAME.TOPIC);
     const topicConstruct = new TopicConstruct(this, topicName, {
       topicName: topicName,
-      emails: [process.env.EMAILS as string],
+      emails: [process.env.EMAILS as string]
     });
 
-    const eventRuleConstruct = new EventRuleConstruct(
-      this,
-      "EventRuleConstruct",
-      {
-        source: "aws.guardduty",
-        detailType: "GuardDuty Finding",
-      }
-    );
+    const eventRuleConstruct = new EventRuleConstruct(this, "EventRuleConstruct", {
+      source: "aws.guardduty",
+      detailType: "GuardDuty Finding"
+    });
 
     eventRuleConstruct.eventRule.addTarget(
       new events_targets.SnsTopic(topicConstruct.topic, {
         // prettier-ignore
-        message: events.RuleTargetInput.fromText(`WARNING: AWS GuardDuty has discovered a ${events.EventField.fromPath('$.detail.type')} security issue for (${events.EventField.fromPath('$.region')}). Please go to https://${events.EventField.fromPath('$.region')}.console.aws.amazon.com/guardduty/ to find out more details.`),
+        message: events.RuleTargetInput.fromText(`WARNING: AWS GuardDuty has discovered a ${events.EventField.fromPath('$.detail.type')} security issue for (${events.EventField.fromPath('$.region')}). Please go to https://${events.EventField.fromPath('$.region')}.console.aws.amazon.com/guardduty/ to find out more details.`)
       })
     );
   }
